52e329a50a733f99e54618459437d9ffb52cfa52
mJiyan <m.jiyan_aslan@hotmail.com>
2021-12-21 18:19:38 +0300

    write the first test

--- __test__/Components/App.test.js
+++ __test__/Components/App.test.js
@@ -3,11 +3,13 @@ import App from '../../src/App';
 import { mount } from 'enzyme';
 import 'jsdom-global/register'; //at the top of file , even  , before importing react
 import toJson from 'enzyme-to-json'; //added this line
+import data from '../../src/fake-data/data';
 
-const wrapper = mount(<App />);
+const wrapper = mount(<App data={data} />);
 
 describe('rendering components', () => {
   test('it should match with the snapshot', () => {
-    expect(toJson(wrapper)).toMatchSnapshot();
+    const header = <p className="lg:text-base md:text-sm sm:text-xs text-white">B</p>;
+    expect(wrapper.contains(header)).toEqual(true);
   });
 });
